41d223c7f36b35c4c24f0bd48e95e91a970532d4
Lorena Silva <lorena.silva@codeminer42.com>
2022-02-03 16:15:44 -0300

    Added tests of validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:age)}
+    it { should validate_presence_of(:bio)}
   end
 end
