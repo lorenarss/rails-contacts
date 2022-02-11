44d2e9de4264fe14d0977b7bbf5e5e11520af16e
Lorena Silva <lorena.silva@codeminer42.com>
2022-02-11 11:43:22 -0300

    commit just to make maerge with branch main, and start doing rails tas for today

--- Gemfile
+++ Gemfile
@@ -59,6 +59,3 @@ end
 gem 'net-smtp', require: false
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
-
-
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,8 +1,8 @@
 class Phone < ApplicationRecord
     belongs_to :contact
 
-    validates :number, presence: true , uniqueness: { scope: :contact_id}
+    validates :number, presence: true , uniqueness: { scope: :contact_id, case_sensitive: false}, numericality: true
     validates :type_of, presence: true
-   # validates :principal, presence: true, uniquiness: { scope: :contact_id } #, length: { minimum: 2 }
-    validates :principal, presence: :true, uniqueness: true
+    # validates :principal, presence: true, uniquiness: { scope: :contact_id } #, length: { minimum: 2 }
+    validates :principal, presence: :true, uniqueness: { case_sensitive: false }
 end
